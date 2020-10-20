09c303eccd8569f09803209b3b0594ed3d1b6d0e
nabihaahmad22 <kingkong13nabiha@gmail.com>
2020-10-20 15:28:24 +0500

    First commit

---
+++ JavaApplication24/src/javaapplication24/JavaApplication24.java
@@ -0,0 +1,15 @@
+
+package javaapplication24;
+
+public class JavaApplication24 {
+
+   
+    public static void main(String[] args) {
+        int num=1;
+        int num2= 3;
+        int sum= num+num2;
+        System.out.println("Sum of two numbers"+":"+sum);
+     
+    }
+    
+}
